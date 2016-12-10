33749859f6f4688ca64d075ee25c07cf62f8875e
kavispires <kavispires@gmail.com>
2016-12-10 11:20:00 -0800

    Add BlackPink to database

--- www/js/database.js
+++ www/js/database.js
@@ -1,50 +1,56 @@
 var DATABASE = [
         {	
-        	'index': 0,
+        	'index' : 0,
         	'name' : 'Nerdstreet Boyz',
-            'members': ['Pat', 'Brian'],
-            'colors': ['red', 'blue']
+            'members' : ['Pat', 'Brian'],
+            'colors' : ['red', 'blue']
         },
         {
-        	'index': 1,
+            'index' : 1,
+            'name' : 'BlackPink',
+            'members' : ['Jisoon', 'Jennie', 'Rosé', 'Lisa'],
+            'colors' : ['forest', 'blue', 'hotpink', 'purple']
+        },
+        {
+        	'index' : 2,
         	'name' : 'KaViS',
-            'members': ['Kyle', 'Anthony', 'Ira', 'Viktor', 'Seth'],
-            'colors': ['blue', 'red', 'violet','yellow', 'green']
+            'members' : ['Kyle', 'Anthony', 'Ira', 'Viktor', 'Seth'],
+            'colors' : ['blue', 'red', 'violet','yellow', 'green']
         },
         {
-        	'index': 2,
+        	'index' : 3,
         	'name' : 'Girls Aloud',
-            'members': ['Nadine', 'Cheryl', 'Kimberly', 'Sarah', 'Nicola'],
-            'colors': ['orange', 'red', 'pink', 'yellow', 'cyan']
+            'members' : ['Nadine', 'Cheryl', 'Kimberly', 'Sarah', 'Nicola'],
+            'colors' : ['orange', 'red', 'pink', 'yellow', 'cyan']
         },
         {
-        	'index': 3,
+        	'index' : 4,
 			'name' : "Girls' Generation",
-            'members': ['Taeyeon', 'Sunny', 'Tiffany', 'Hyoyeon', 'Yuri', 'Sooyong', 'Yonna', 'Seohyun'],
-            'colors': ['orange', 'purple', 'pink', 'green', 'red', 'cyan', 'yellow', 'teal']
+            'members' : ['Taeyeon', 'Sunny', 'Tiffany', 'Hyoyeon', 'Yuri', 'Sooyong', 'Yonna', 'Seohyun'],
+            'colors' : ['orange', 'purple', 'pink', 'green', 'red', 'cyan', 'yellow', 'teal']
         },
         {
-        	'index': 4,
+        	'index' : 5,
 			'name' : 'Twice',
-            'members': ['Nayeon', 'Jeongyeon', 'Momo', 'Sana', 'Jihyo', 'Mina', 'Dahyun', 'Chaeyoung', 'Tzuyu'],
-            'colors': ['orange', 'green', 'red', 'blue', 'purple', 'violet', 'pink', 'cyan', 'yellow'],
+            'members' : ['Nayeon', 'Jeongyeon', 'Momo', 'Sana', 'Jihyo', 'Mina', 'Dahyun', 'Chaeyoung', 'Tzuyu'],
+            'colors' : ['orange', 'green', 'red', 'blue', 'purple', 'violet', 'pink', 'cyan', 'yellow'],
         },
         {
-        	'index': 5,
+        	'index' : 6,
 			'name' : 'Squirrel Zone',
-            'members': ['Leo', 'Bryan', '3J', 'Bobak', 'Zack', 'Nathan', 'JQ', 'Sypher', 'Nicky', 'Robbie'],
+            'members' : ['Leo', 'Bryan', '3J', 'Bobak', 'Zack', 'Nathan', 'JQ', 'Sypher', 'Nicky', 'Robbie'],
             'colors': ['teal', 'violet', 'orange', 'red', 'navy', 'green', 'yellow', 'purple', 'pink', 'grey']
         },
         {
-        	'index': 6,
+        	'index' : 7,
 			'name' : 'Super Junior',
-            'members': ['Leeteuk', 'Heechul', 'Yesung', 'Kangin', 'Shindong', 'Sungmin', 'Eunhyuk', 'Donghae', 'Siwon', 'Ryeowook', 'Kyuhyun'],
-            'colors': ['yellow', 'orange', 'red', 'violet', 'pink', 'purple', 'cyan', 'blue', 'teal', 'green', 'grey']
+            'members' : ['Leeteuk', 'Heechul', 'Yesung', 'Kangin', 'Shindong', 'Sungmin', 'Eunhyuk', 'Donghae', 'Siwon', 'Ryeowook', 'Kyuhyun'],
+            'colors' : ['yellow', 'orange', 'red', 'violet', 'pink', 'purple', 'cyan', 'blue', 'teal', 'green', 'grey']
         },
         {
-        	'index': 7,
+        	'index' : 8,
 			'name' : 'K-Aliens',
-			'members': ['Kalvin','Andy','Vincent','Sammy','Max','Leroy','Isaac','Sunny','Ike','Ethan','Gareth','Valentino','Alex','Shela','Drob'],
-			'colors': ['green','purple','blueviolet','yellow','hotpink','blue','red','orange','pink','teal','grey','forest','brown','cyan','black'],
-        },
+			'members' : ['Kalvin','Andy','Vincent','Sammy','Max','Leroy','Isaac','Sunny','Ike','Ethan','Gareth','Valentino','Alex','Shela','Drob'],
+			'colors' : ['green','purple','blueviolet','yellow','hotpink','blue','red','orange','pink','teal','grey','forest','brown','cyan','black'],
+        }
 ];
